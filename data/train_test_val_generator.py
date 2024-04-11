d10a6e4ed46b2e889456f05927dbb14064d1b258
Elian Daniel Foppiano <efoppiano@fi.uba.ar>
2024-04-11 20:26:17 -0300

    Add train_test_val_generator.py

---
+++ data/train_test_val_generator.py
@@ -0,0 +1,27 @@
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+def main():
+    df = pd.read_csv("housing.csv")
+    X = df.drop("median_house_value", axis=1)
+    y = df["median_house_value"]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+    
+    X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.25, random_state=1)
+    
+    X_train = pd.DataFrame(X_train, columns=X_train.columns)
+    X_train["median_house_value"] = y_train
+    
+    X_test = pd.DataFrame(X_test, columns=X_test.columns)
+    X_test["median_house_value"] = y_val
+    
+    X_val = pd.DataFrame(X_val, columns=X_val.columns)
+    X_val["median_house_value"] = y_val
+
+    X_train.to_csv("train.csv")
+    X_test.to_csv("test.csv")
+    X_val.to_csv("validation.csv")
+
+if __name__ == "__main__":
+    main()
